92d431627ef4eedd1686d5370f04bd68cec547bb
Gilangaskari <ragilangaskari@gmail.com>
2023-09-25 02:17:12 +0800

    tugas ukm

---
+++ if_els.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int uang,harga,kembalian,jam_belanja,hasil;
+cout<<"minimal uang :";
+cin>>uang;
+cout<<"harga barang :";
+cin>>harga;
+cout<<"jam belanja ";
+cin>>jam
+if ( uang > harga )
+{
+   hasil = uang - harga jam_belanja;
+   cout<<"kembalian ="<<kembalian;
+
+}
+else if (uang < harga){
+
+    cout<<""
+}
+else 
+{
+    cout <<" terimakasih sudah belanja ";
+}
+}
